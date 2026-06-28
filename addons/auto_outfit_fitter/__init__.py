1fdc83a3705724c02d9e2f447b12d9602e012151
pktarzen <34571977+pktarzen@users.noreply.github.com>
2026-06-28 19:34:46 +0530

    Update addon metadata and registration methods

--- addons/auto_outfit_fitter/__init__.py
+++ addons/auto_outfit_fitter/__init__.py
@@ -1,18 +1,41 @@
 bl_info = {
     "name": "PM Auto Outfit Fitter",
     "author": "PM Animation Studio",
-    "version": (1,0,0),
-    "blender": (4,0,0),
+    "version": (1, 0, 0),
+    "blender": (4, 0, 0),
     "location": "View3D > Sidebar > PM Tools",
-    "description": "Automatic outfit fitting",
+    "description": "Fit selected clothes to active body mesh",
     "category": "Object",
 }
 
+import bpy
+
+from .operators import (
+    PMOF_OT_AnalyzeBody,
+    PMOF_OT_FitClothes,
+)
+
+from .ui import (
+    PMOF_PT_Panel,
+)
+
+
+classes = (
+    PMOF_OT_AnalyzeBody,
+    PMOF_OT_FitClothes,
+    PMOF_PT_Panel,
+)
+
+
 def register():
-    pass
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
 
 def unregister():
-    pass
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
 
 if __name__ == "__main__":
     register()
