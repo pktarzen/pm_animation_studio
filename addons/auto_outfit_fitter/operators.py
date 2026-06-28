242d845d268e5169a45273a6755ddbc5a165e1bc
pktarzen <34571977+pktarzen@users.noreply.github.com>
2026-06-28 19:33:01 +0530

    Update operators.py

--- addons/auto_outfit_fitter/operators.py
+++ addons/auto_outfit_fitter/operators.py
@@ -1,5 +1,47 @@
-# PM Auto Outfit Fitter Operators
-
 import bpy
 
-# Operators will be added here.
+from .utils import analyze_body, fit_clothes
+
+
+class PMOF_OT_AnalyzeBody(bpy.types.Operator):
+    bl_idname = "pmof.analyze_body"
+    bl_label = "Analyze Body"
+
+    def execute(self, context):
+
+        body = context.active_object
+
+        if body is None:
+            self.report({'ERROR'}, "Select body mesh")
+            return {'CANCELLED'}
+
+        w, d, h = analyze_body(body)
+
+        self.report(
+            {'INFO'},
+            f"H={h:.2f}  W={w:.2f}  D={d:.2f}"
+        )
+
+        return {'FINISHED'}
+
+
+class PMOF_OT_FitClothes(bpy.types.Operator):
+    bl_idname = "pmof.fit_clothes"
+    bl_label = "Fit Selected Clothes"
+
+    def execute(self, context):
+
+        body = context.active_object
+
+        if body is None:
+            self.report({'ERROR'}, "Select body mesh")
+            return {'CANCELLED'}
+
+        fit_clothes(body)
+
+        self.report(
+            {'INFO'},
+            "Clothes fitting completed"
+        )
+
+        return {'FINISHED'}
