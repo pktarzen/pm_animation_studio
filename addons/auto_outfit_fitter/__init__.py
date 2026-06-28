adbcc86bc02919c65cfc5de02da7502ae0e9f041
pktarzen <34571977+pktarzen@users.noreply.github.com>
2026-06-28 19:24:01 +0530

    Add PM Auto Outfit Fitter add-on
    Initial implementation of the PM Auto Outfit Fitter add-on for Blender.

---
+++ addons/auto_outfit_fitter/__init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "PM Auto Outfit Fitter",
+    "author": "PM Animation Studio",
+    "version": (1,0,0),
+    "blender": (4,0,0),
+    "location": "View3D > Sidebar > PM Tools",
+    "description": "Automatic outfit fitting",
+    "category": "Object",
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
